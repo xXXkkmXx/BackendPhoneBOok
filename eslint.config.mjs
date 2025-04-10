565d9ea142eba128f16ffbbb0e94cd4b7d487e8d
Maxymilian Kowalczyk <kowalczykm@tlimc.local>
2025-04-10 15:24:43 +0200

    added eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,25 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
+import js from '@eslint/js'
+import stylistcJs from '@stylistic/eslint-plugin-js'
 
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
-]);
+export default defineConfig(
+  js.configs.recommended,
+  [
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+    
+  ],
+  {
+    plugins:{
+      '@stylistic/js' : stylistcJs
+    },
+    rules:{
+      '@stylistic/js/indent' : ['error',2],
+      '@stylistic/js/linebreak-style' : ['error','unix'],
+      '@stylistic/js/qoutes' : ['error','single'],
+      '@stylistic/js/semi' : ['error','never']
+    }
+  }
+);
